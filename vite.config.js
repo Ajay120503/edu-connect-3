1a3bced9fbb12b39ddf2b1f237416ef3f67f4866
Ajay120503 <ajaykandhare12@gmail.com>
2026-07-29 18:07:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,59 +3,63 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons/icon-192.png', 'icons/icon-512.png'],
-      manifest: {
-        name: 'EduConnect',
-        short_name: 'EduConnect',
-        description: 'Academic Social Network - Where Academic Careers Begin',
-        theme_color: '#4F46E5',
-        background_color: '#0F172A',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'],
-        navigateFallback: '/index.html',
-        navigateFallbackDenylist: [/^\/api\//, /^\/socket.io\//],
-        navigateFallbackAllowlist: [/^\/[^.]*$/],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+    ...(mode === 'production'
+      ? [
+          VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'icons/icon-192.png', 'icons/icon-512.png'],
+            manifest: {
+              name: 'EduConnect',
+              short_name: 'EduConnect',
+              description: 'Academic Social Network - Where Academic Careers Begin',
+              theme_color: '#4F46E5',
+              background_color: '#0F172A',
+              display: 'standalone',
+              start_url: '/',
+              icons: [
+                {
+                  src: 'icons/icon-192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+                },
+                {
+                  src: 'icons/icon-512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                },
+              ],
             },
-          },
-          {
-            urlPattern: /^https:\/\/res\.cloudinary\.com\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'cloudinary-cache',
-              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 7 },
+            workbox: {
+              globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'],
+              navigateFallback: '/index.html',
+              navigateFallbackDenylist: [/^\/api\//, /^\/socket.io\//],
+              navigateFallbackAllowlist: [/^\/[^.]*$/],
+              runtimeCaching: [
+                {
+                  urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                  handler: 'CacheFirst',
+                  options: {
+                    cacheName: 'google-fonts-cache',
+                    expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+                  },
+                },
+                {
+                  urlPattern: /^https:\/\/res\.cloudinary\.com\/.*/i,
+                  handler: 'NetworkFirst',
+                  options: {
+                    cacheName: 'cloudinary-cache',
+                    expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 7 },
+                  },
+                },
+              ],
             },
-          },
-        ],
-      },
-    }),
+          }),
+        ]
+      : []),
   ],
   server: {
     port: 5173,
@@ -70,4 +74,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
